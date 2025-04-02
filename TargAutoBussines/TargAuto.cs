bafa7ebab57b068356f8dff7d5c96b0d5cb96e34
Dumencu <dumencu.catalin1@gmail.com>
2025-04-02 12:15:30 +0300

    Add files via upload

---
+++ TargAutoBussines/TargAuto.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using TargAutoLibrary;
+
+namespace TargAutoBusiness
+{
+    public class TargAuto
+    {
+        private List<Masina> masini = new List<Masina>(); // lista interna masini
+
+        public void AddMasina(Masina m) // adauga masina
+        {
+            masini.Add(m);
+        }
+
+        public List<Masina> GetMasini() // return toate masinile
+        {
+            return masini;
+        }
+
+        public List<Masina> CautaMasinaDupaMarca(string marca) // cauta masina dupa marca
+        {
+            List<Masina> rezultat = new List<Masina>();
+            foreach (var m in masini)
+            {
+                if (m.Marca.ToLower() == marca.ToLower()) // cautar insensibila litere mari mici
+                    rezultat.Add(m);
+            }
+            return rezultat;
+        }
+    }
+}
